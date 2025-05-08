b148ba9d19fe393914c136fec8354122179035cf
Escuela-Manejo-G-25 <na.sangchi5.0@gmail.com>
2025-05-08 01:47:19 -0300

    Delete js directory

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-  const header = document.querySelector('.hero');
-  if (window.scrollY > 50) {
-    header.classList.add('shrink');
-  } else {
-    header.classList.remove('shrink');
-  }
-});
