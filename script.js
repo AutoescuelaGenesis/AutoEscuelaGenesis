e0588cebac27ba7ac1b80261dd983157724ded34
Escuela-Manejo-G-25 <210767998+Escuela-Manejo-G-25@users.noreply.github.com>
2025-05-11 18:47:31 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-setTimeout(() => {
-  const msg = document.getElementById('whatsapp-msg');
-  msg.style.display = 'inline-block';
-  setTimeout(() => { msg.style.display = 'none'; }, 60000);
-}, 120000);
